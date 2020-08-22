7af80eebbf0502fcdce637aa485269df3cb70e02
Denisa Andron <denn.metal@gmail.com>
2020-08-22 21:23:35 +0300

    html reports

--- Drivers/Drivers.py
+++ Drivers/Drivers.py
@@ -1,4 +1,6 @@
 
 
 class executables:
-    CHROME_EXECUTABLE_PATH = "C:\\Users\\Denisa\Desktop\\selenium\\facebook\drivers\\chromedriver.exe"
+    CHROME_EXECUTABLE_PATH = "C:\\Users\\Denisa\\Desktop\\selenium\\nopCommerce\\Drivers\\chromedriver.exe"
+    FIREFOX_EXECUTABLE_PATH = "C:\\Users\\Denisa\\Desktop\\selenium\\nopCommerce\\Drivers\\geckodriver.exe"
+    EDGE_EXECUTABLE_PATH = "C:\\Users\\Denisa\\Desktop\\selenium\\nopCommerce\\Drivers\\msedgedriver.exe"
