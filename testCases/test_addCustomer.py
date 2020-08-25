1e239cc38f6d5d7df2d97cc4543febe335245109
Denisa Andron <denn.metal@gmail.com>
2020-08-25 09:48:51 +0300

    add customer

---
+++ testCases/test_addCustomer.py
@@ -0,0 +1,91 @@
+import pytest
+import time
+from pageObjects.LoginPage import LoginPage
+from pageObjects.AddCustomerPage import AddCustomer
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+import string
+import random
+
+class Test_003_AddCustomer:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUsername()
+    password = ReadConfig.getPassword()
+
+    customerPassword = ReadConfig.getCustomerPassword()
+    customerRoles = ReadConfig.getCustomerRoles()
+    vendor = ReadConfig.getVendor()
+    gender = ReadConfig.getGender()
+    firstName = ReadConfig.getFirstName()
+    lastName = ReadConfig.getLastName()
+    dob = ReadConfig.getDateOfBirth()
+    company = ReadConfig.getCompany()
+    adminContent = ReadConfig.getAdminContent()
+
+
+    logger = LogGen.loggen()
+
+
+    def test_addCustomer(self, setUp):
+        self.logger.info("***** Test_003_AddCustomer****")
+        self.driver = setUp
+        self.driver.get(self.baseURL)
+        self.driver.maximize_window()
+
+        self.lp = LoginPage
+
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        self.logger.info("*** Login succesful ****")
+
+        self.logger.info("**** Starting Add Customer Test***")
+
+        self.addcust = AddCustomer(self.driver)
+        self.addcust.clickOnCustomersMenu()
+        self.addcust.clickOnCustomersMenuItem()
+
+        self.addcust.clickOnAddnew()
+
+
+
+        self.logger.info("*** Providing customer info ***")
+
+        self.email = random_generator() + "@gmail.com"
+        time.sleep(2)
+        self.addcust.setEmail(self.email)
+        self.addcust.setCustomerPassword(self.customerPassword)
+        self.addcust.setCustomerRoles(self.customerRoles)
+        self.addcust.setManagerOfVendor(self.vendor)
+        self.addcust.setGender(self.gender)
+        self.addcust.setFirstName(self.firstName)
+        self.addcust.setLastName(self.lastName)
+        self.addcust.setDob(self.dob)
+        self.addcust.setCompanyName(self.company)
+        self.addcust.setAdminContent(self.adminContent)
+        self.addcust.clickOnSave()
+
+        self.logger.info("**** Saving customer info *****")
+
+        self.logger.info("*** Add customer validation started ****")
+
+        self.msg = self.driver.find_element_by_tag_name("body").text
+
+        print(self.msg)
+
+        if 'customer has been added successfuly.' in self.msg:
+            assert True == True
+            self.logger.info("***** Add customer Test Passed ****")
+
+        else:
+            self.driver.save_screenshot(".\\Screenshots\\" + "test_addCustomer_scr.png")
+            self.logger.error("*** Add customer Test Failed ***")
+            assert True == False
+
+        self.driver.close()
+        self.logger.info(" **** Ending Home Page Title Test ****")
+
+
+def random_generator(size=8, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
--- utilities/readProperties.py
+++ utilities/readProperties.py
@@ -22,3 +22,41 @@ class ReadConfig:
     def getPassword():
         password = config.get('common info', 'password')
         return password
+
+    #add customer page
+
+    @staticmethod
+    def getCustomerPassword():
+        customerPassword = config.get('add customer info', 'customerPassword')
+
+    @staticmethod
+    def getCustomerRoles():
+        customerRoles = config.get('add customer info', 'customerRoles')
+
+    @staticmethod
+    def getVendor():
+        vendor = config.get('add customer info', 'vendor')
+
+    @staticmethod
+    def getGender():
+        gender = config.get('add customer info', 'gender')
+
+    @staticmethod
+    def getFirstName():
+        firstname = config.get('add customer info', 'firstname')
+
+    @staticmethod
+    def getLastName():
+        lastname = config.get('add customer info', 'lastname')
+
+    @staticmethod
+    def getDateOfBirth():
+        getdob = config.get('add customer info', 'setdob')
+
+    @staticmethod
+    def getCompany():
+        company = config.get('add customer info', 'company')
+
+    @staticmethod
+    def getAdminContent():
+        adminContent = config.get('add customer info', 'adminContent')
